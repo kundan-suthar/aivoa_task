8d25090016ccd7a08f8f66966b7372ca552b9376
Kundan suthar <kundanss22@gmail.com>
2026-04-16 16:09:50 +0530

    fastapi pproject initialization

---
+++ backend/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow frontend to talk to backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Backend is running!"}
+
